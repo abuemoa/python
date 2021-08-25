87b4a55696ce2edf8947c9cb9dd10ab2dfb9d621
abuemoa <88618807+abuemoa@users.noreply.github.com>
2021-08-25 12:29:41 +0200

    Create ageHours.py

---
+++ ageHours.py
@@ -0,0 +1,6 @@
+#programa sencillo que muestra las horas vividas basado en la edad total
+day = float(24)
+year = float(day*365.25)
+age = float(input("How old are you? "))
+last = age*year
+print("You are", last, "hours old")
