15eee29cae200e12e8348d19109793a7db451e58
abuemoa <88618807+abuemoa@users.noreply.github.com>
2021-08-26 10:22:33 +0200

    Update yearsMonthsDaysAge.py

--- yearsMonthsDaysAge.py
+++ yearsMonthsDaysAge.py
@@ -1,9 +1,16 @@
 import datetime
+import time
+
 year = int(input("Introduzca su año de nacimiento: "))
 month = int(input("Introduzca su mes de nacimiento. ej. Agosto = 8: "))
 day = int(input("Introduzca su día de nacimiento: "))
-currentTime = datetime.datetime.now()
-currentArray = [currentTime.year, currentTime.month, currentTime.day]
-birthDate = (year, month, day)
-myDate = [e1 - e2 for e1, e2 in zip(currentArray,birthDate)]
-print("Tienes", myDate[0], "años,", myDate[1], "meses y", myDate[2], "días")
+
+currentTime = time.time()
+givenTime = datetime.datetime(year, month, day, 0, 0).timestamp()
+difference = currentTime - givenTime
+
+yearsOld = int((time.strftime('%Y', time.localtime(difference))))
+MonthsOld = int((time.strftime('%m', time.localtime(difference))))
+DaysOld = int((time.strftime('%d', time.localtime(difference))))
+
+print("Tienes", (yearsOld - 1970), "años", MonthsOld - 1, "meses y", DaysOld - 1, "días")
