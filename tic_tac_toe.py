4faf6e9b28aa424c74ab8d9e267978dd38e4d2fb
Alejandro <88618807+abuemoa@users.noreply.github.com>
2021-09-07 09:50:22 +0200

    Create tic_tac_toe.py

---
+++ tic_tac_toe.py
@@ -0,0 +1,28 @@
+import random
+
+end_of_game = ""
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's' or player == 'p' and opponent == 'r' or player == 's'\
+        or opponent == 'p'):
+        return True 
+
+while end_of_game == "y" or end_of_game == "":
+    def play():
+        user = input("'r' for rock, 'p' for paper and 's' for scissors:\n")
+        computer = random.choice(['r', 'p', 's'])
+
+        if user == computer:
+            return "It's a tie"
+        elif is_win(user,computer):
+            return "you won"
+    print(play())
+    end_of_game = input("If you don't want to continue playing this game, press the button 'n'\n")
+    if end_of_game == "y" or end_of_game == "Y":
+        end_of_game = "y"
+    elif end_of_game == "n" or end_of_game == "N": 
+        end_of_game = "n"
+    else:
+        end_of_game = "y"
+        
+
+
