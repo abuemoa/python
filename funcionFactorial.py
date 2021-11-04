5ee5c0c59b6aa5461c6a2e912120a4f809a7ded4
Alejandro <88618807+abuemoa@users.noreply.github.com>
2021-11-04 09:49:39 +0100

    Gracias a Blauma otra vez
    Ahora lo que he hecho ha sido empezar el rango de números por el 1, de tal manera que cuando se añada el 1 salte un 1 y no tenga que poner la excepción como antes, cuando el rango empezaba por 2.
    También he eliminado el or que había dentro del condicional. Ya no es necesario.

--- funcionFactorial.py
+++ funcionFactorial.py
@@ -3,11 +3,10 @@ print("Introduzca la función factorial que quiere realizar")
 operador = int(input())
 print(f"Ha elegido el número {operador}")
 numero = operador
-if numero == 0 or numero == 1:
+if numero == 0:
     operador = 1
 else:
-    for i in range(2,operador):
+    for i in range(1,operador):
         operador *= i
 
-print(f"El resultado de la función factorial de {numero} es {operador}")
-
+print(f"El resultado de la función factorial de {numero} es {operador}")                                                                        
